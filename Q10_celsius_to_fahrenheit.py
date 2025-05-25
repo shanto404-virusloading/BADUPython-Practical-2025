5e9d978255b1bb287b2886257aced81cd2605487
Sh@nto <the404networkerror@gmail.com>
2025-05-26 02:02:56 +0600

    Add files via upload

---
+++ Q10_celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+# Write a program to convert temperature from Celsius to Fahrenheit.
+
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"tenperature celsius to fahrenheit is {fahrenheit}")
