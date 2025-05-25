d93b546bfc8f31d58363fade46ae61a56bbd93a3
Sh@nto <the404networkerror@gmail.com>
2025-05-26 01:54:10 +0600

    Create Q5_larger_number.py

---
+++ Q5_larger_number.py
@@ -0,0 +1,11 @@
+# Write a program that finds the larger of two numbers.
+
+
+A = int(input("enter first number"))
+B = int(input("enter second number"))
+if (A==B):
+    print("the two number is same")
+elif (A>B):
+    print("the number {A} is largest")
+else:
+    print("the number {B} is largest")
