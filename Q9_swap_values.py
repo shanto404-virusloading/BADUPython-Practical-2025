713b462b0390b133adbd688ec921fbc3c33ab211
Sh@nto <the404networkerror@gmail.com>
2025-05-26 02:01:27 +0600

    Add files via upload

---
+++ Q9_swap_values.py
@@ -0,0 +1,11 @@
+# Write a program to swap the values of two variables.
+
+
+a = input("Enter the first value ")
+b = input("Enter the second value ")
+
+a, b = b, a
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
