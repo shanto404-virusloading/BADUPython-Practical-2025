9fe4840970e39a3a148ada85dfe85017f3680d40
Sh@nto <the404networkerror@gmail.com>
2025-05-26 02:00:26 +0600

    Create Q8_pos_neg_zero.py

---
+++ Q8_pos_neg_zero.py
@@ -0,0 +1,10 @@
+# Write a program to check whether a number is positive, negative, or zero.
+
+
+A = int(input("enter a number"))
+if (A>0):
+    print("the number {A} is positive")
+elif (A<0):
+    print("the number {A} is negative")
+else:
+    print("the number is zero")
