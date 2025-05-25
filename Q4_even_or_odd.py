c5521f37d80c5f61c955edd21716b1ec563b9a94
Sh@nto <the404networkerror@gmail.com>
2025-05-26 01:51:16 +0600

    Create Q4_even_or_odd.py

---
+++ Q4_even_or_odd.py
@@ -0,0 +1,10 @@
+# Write a program that checks whether a number is even or odd.
+
+
+A = int(input("enter a number"))
+if (A==0):
+    print("the number is zero")
+elif (A%2==0):
+    print(f"the number {A} is even")
+else:
+    print(f"the number {A} is odd")
