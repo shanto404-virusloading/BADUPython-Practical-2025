7141681084cfa4fa98faab744a68c87a8717d174
Sh@nto <the404networkerror@gmail.com>
2025-05-26 02:18:35 +0600

    Update Q4_even_or_odd.py

--- Q4_even_or_odd.py
+++ Q4_even_or_odd.py
@@ -5,6 +5,6 @@ A = int(input("enter a number"))
 if (A==0):
     print("the number is zero")
 elif (A%2==0):
-    print(f"the number {A} is even")
+    print("the number {A} is even")
 else:
-    print(f"the number {A} is odd")
+    print("the number {A} is odd")
